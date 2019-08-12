1dae040dc842b59bac68a10abf2eaf01f592b65b
abhi1381 <abhilnm011@gmail.com>
2019-08-12 14:25:24 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink } from 'reactstrap';
-
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
+import "./App.css";
+import Menu from "./components/MenuComponent";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="dark">
         <div className="container">
           <NavbarBrand href="/">SweetsHub</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }

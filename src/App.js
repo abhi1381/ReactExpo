78d559108b6514e98a17de2866f256813d578a36
abhi1381 <abhilnm011@gmail.com>
2019-08-12 13:33:02 +0530

    React configuration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar dark color="dark">
+        <div className="container">
+          <NavbarBrand href="/">SweetsHub</NavbarBrand>
+        </div>
+      </Navbar>
     </div>
   );
 }

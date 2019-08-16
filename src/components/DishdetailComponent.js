0eba8ef57b9a87fdf5ca026b9d684413333aa48e
abhi1381 <abhilnm011@gmail.com>
2019-08-16 23:50:38 +0530

    React-Router

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,77 +1,67 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
+function RenderDish({ dish }) {
+  // console.log(dish);
+  if (dish != null) {
+    return (
+      <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  } else {
+    return <div />;
+  }
+}
 
+function RenderComments({ dish }) {
+  if (dish != null) {
+    let layoutComment = dish.comments.map(Comment => {
+      return (
+        <li key={Comment.id} className="list-unstyled">
+          <div>
+            <p>{Comment.comment}</p>
+            <p>
+              -- {Comment.author} ,{" "}
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit"
+              }).format(new Date(Date.parse(Comment.date)))}
+            </p>
+          </div>
+        </li>
+      );
+    });
+    return (
+      <ul>
+        <h4>Comments</h4>
+        {layoutComment}
+      </ul>
+    );
+  } else {
+    return <div />;
+  }
+}
 
-    function RenderDish({dish}) {
-        // console.log(dish);
-        if(dish != null) {
-            return (
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card> 
-                    
-                );
-        } else {
-            return (
-                <div></div>
-            );
-        }
-    }
+const DishDetail = props => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          <RenderDish dish={props.dish[0]} />
+        </div>
 
-    
+        <div className="col-12 col-md-5 m-1">
+          <RenderComments dish={props.dish[0]} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-    function RenderComments({dish} ) {
-        if(dish != null) {
-            let layoutComment = dish.comments.map((Comment) => {
-                return ( 
-                        <li key = {Comment.id} className="list-unstyled"> 
-                            <div>
-                                <p>{Comment.comment}</p>
-                                <p>-- {Comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(Comment.date)))}</p>
-                            </div>
-                        </li>
-                    
-                    )
-                }
-            );
-            return (
-                <ul>
-                    <h4>Comments</h4>
-                    {layoutComment}
-                </ul>
-            );
-        } else {
-            return (
-                <div></div>
-            )
-        }
-    }
-    
-
-
-    const DishDetail = (props) => {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish[0]} />                   
-                    </div>
-                    
-                    <div  className="col-12 col-md-5 m-1">
-                        <RenderComments dish={props.dish[0]} />                      
-                    </div>                        
-                </div>
-            </div>
-        );
-    }
-
-        
-    
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,42 +1,45 @@
 // pseudo container component for the application
 
-import React, {
-    Component
-  } from "react";
-  
-  import DishDetail from "./DishdetailComponent";
-  import Menu from "./MenuComponent";
-  import Header from "./HeaderComponent";
-  import Footer from "./FooterComponent";
-  import {
-    DISHES
-  } from '../shared/dishes';
-  
-  class Main extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        dishes: DISHES,
-        selectedDish: null
-      }
-    }
+import React, { Component } from "react";
 
-    onDishSelect(dishId) {
-        this.setState({
-            selectedDish: dishId
-        });
-    }
-    render() {
-        // console.log(this.state.dishes);
-      return (
-        <div>
-          <Header/>
-          <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
-          <DishDetail dish= {this.state.dishes.filter(dish => dish.id === this.state.selectedDish)}/>
-          <Footer/>
-        </div>
-      );
-    }
+import DishDetail from "./DishdetailComponent";
+import Home from "./HomeComponent";
+import Menu from "./MenuComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import { DISHES } from "../shared/dishes";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
   }
-  
-  export default Main;
+
+  render() {
+    // console.log(this.state.dishes);
+    const HomePage = () => {
+      return <Home />;
+    };
+
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Main;
